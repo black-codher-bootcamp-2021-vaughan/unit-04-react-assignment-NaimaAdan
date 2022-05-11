c74c6b552e0a593a38a4205f730255755204c569
NaimaAdan <nai.adan@outlook.com>
2022-05-11 21:25:30 +0100

    React complete

---
+++ src/components/BasketTotal.js
@@ -0,0 +1,19 @@
+const BasketTotal = ({ items }) => {
+    // save a const with the total cost
+    // items.map(item => item.price)
+    // an array with all the numbers for the prices
+    // sum all the item prices
+    let totalPrice = 0;
+    console.log("----items", items);
+  
+    items.map((item) => {
+      console.log(item.trackPrice);
+      console.log(totalPrice);
+  
+      return (totalPrice = totalPrice + item.trackPrice);
+    });
+  
+    return <div></div>;
+  };
+  
+  export default BasketTotal;
